1830491d2867a8f874d372b2f474bdada72cbe9c
lassesteffen <lasse.steffen@t-online.de>
2019-05-18 16:43:24 +0200

    Image taking

--- lib/screens/tabs/profile.dart
+++ lib/screens/tabs/profile.dart
@@ -1,7 +1,75 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Profile extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Profile extends StatefulWidget {
+  ProfileState createState() => ProfileState();
+}
+
+class ProfileState extends State<Profile> {
+  String profileImage = 'assets/profile.jpeg';
+
   Widget build(BuildContext context) {
-    return Text('Profile');
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Image.asset(
+                profileImage,
+                height: 280,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: FlatButton(
+                  child: Icon(Icons.edit),
+                  onPressed: () {
+                    _takePicture(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text('Africa, Simbabwe'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text(
+              'Skills',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          ListTile(
+            title: Text('Skill 1'),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.message),
+            title: Text(
+              'Needs',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          ListTile(
+            title: Text('Need 1'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _takePicture(BuildContext context) async {
+    File cameraFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+    );
+    setState(() {
+      profileImage = cameraFile.path;
+    });
   }
 }
